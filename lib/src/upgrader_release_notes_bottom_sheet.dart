26ca14e5d6a6e07f5eb0893f7f9c32769bfa7141
remotr <dev@remoteconsult.dk>
2024-12-03 15:38:56 +0100

    CircularProgressIndicator added using StatefulBuilder

--- lib/src/upgrader_release_notes_bottom_sheet.dart
+++ lib/src/upgrader_release_notes_bottom_sheet.dart
@@ -21,13 +21,8 @@ class UpgraderReleaseNotesBottomSheet {
     TextStyle? bottomSheetTitleTextStyle,
     TextStyle? bottomSheetReleaseNotesTextStyle,
   }) async {
-    Upgrader upgrader = Upgrader(languageCode: languageCode);
-
-    await upgrader.initialize();
-
-    final versionInfo = await upgrader.updateVersionInfo();
-    final releaseNotes = versionInfo?.releaseNotes;
-
+    bool loading = true;
+    String? releaseNotes;
     if (context.mounted) {
       await showModalBottomSheet(
         backgroundColor: bottomSheetBackgroundColor ?? Colors.white,
@@ -35,47 +30,75 @@ class UpgraderReleaseNotesBottomSheet {
         useSafeArea: true,
         isScrollControlled: true,
         builder: (BuildContext c) {
-          return ConstrainedBox(
-            constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height *
-                    bottomSheetMaxHeightFactor),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(
-                      top: 20, right: 20, left: 20, bottom: 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        UpgraderMessages(code: languageCode).newInThisVersion,
-                        style: bottomSheetTitleTextStyle ??
-                            const TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                ),
-                Flexible(
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.only(
-                        top: 10, right: 20, left: 20, bottom: 50),
-                    child: Text(
-                      style: bottomSheetReleaseNotesTextStyle ??
-                          const TextStyle(fontSize: 14),
-                      releaseNotes ??
-                          UpgraderMessages(code: languageCode)
-                              .noAvailableReleaseNotes,
+          return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setState) {
+            if (loading) {
+              getReleaseNotes(languageCode).then((newReleaseNotes) {
+                setState(() {
+                  loading = false;
+                  releaseNotes = newReleaseNotes;
+                });
+              });
+            }
+
+            return loading
+                ? const SizedBox(
+                    height: 200,
+                    child: Center(child: CircularProgressIndicator()))
+                : ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height *
+                            bottomSheetMaxHeightFactor),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              top: 20, right: 20, left: 20, bottom: 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                UpgraderMessages(code: languageCode)
+                                    .newInThisVersion,
+                                style: bottomSheetTitleTextStyle ??
+                                    const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Flexible(
+                          child: SingleChildScrollView(
+                            padding: const EdgeInsets.only(
+                                top: 10, right: 20, left: 20, bottom: 50),
+                            child: Text(
+                              style: bottomSheetReleaseNotesTextStyle ??
+                                  const TextStyle(fontSize: 14),
+                              releaseNotes ??
+                                  UpgraderMessages(code: languageCode)
+                                      .noAvailableReleaseNotes,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ),
-              ],
-            ),
-          );
+                  );
+          });
         },
       );
     }
+  }
+
+  static Future<String?> getReleaseNotes(languageCode) async {
+    Upgrader upgrader = Upgrader(languageCode: languageCode);
+
+    await upgrader.initialize();
+
+    final versionInfo = await upgrader.updateVersionInfo();
+
+    return versionInfo?.releaseNotes;
   }
 }
